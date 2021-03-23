4ee22c0be4b61870f3424031cf0edc0f9e10c6c9
seif-alhorani <horanisaif5@gmail.com>
2021-03-23 19:56:52 +0200

    python 3

--- macChanger.py
+++ macChanger.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface=input("interface >")
-new_mac=input("new macaddress >")
-print("[+] change interface mac address for "+interface+" to "+new_mac)
-subprocess.call(["ifconfig", interface , "down"])
-subprocess.call(["ifconfig", interface , "hw", "ether" , new_mac])
-subprocess.call(["ifconfig", interface , "up"])
+def get_argument():
+    parser = optparse.OptionParser()
+    parser.add_option("--interface", dest="interface", help="select interface")
+    parser.add_option("--mac", dest="new_mac", help="enter the new mac ")
+    (option, arguments) =parser.parse_args()
+    if not option.interface:
+        #code to handel error
+        parser.error("no interface use --help")
+    elif not option.new_mac:
+        # code to handel error
+        parser.error("no mac use --help")
+    return option
 
 
+def change_mac(interface,new_mac):
+    print("[+] change interface mac address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+option =get_argument()
+change_mac(option.interface,option.new_mac)
