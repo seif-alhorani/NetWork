9e3c46ff80defa5bd682ebcde4d061d1aff94e40
seif-alhorani <horanisaif5@gmail.com>
2021-03-23 12:50:34 +0200

    python 3

--- macChanger.py
+++ macChanger.py
@@ -5,8 +5,8 @@ import subprocess
 interface=input("interface >")
 new_mac=input("new macaddress >")
 print("[+] change interface mac address for "+interface+" to "+new_mac)
-subprocess.call(["ifconfig", interface ,"down"])
-subprocess.call(["ifconfig", interface ,"hw ","ether", new_mac])
-subprocess.call(["ifconfig", interface ,"up"])
+subprocess.call(["ifconfig", interface , "down"])
+subprocess.call(["ifconfig", interface , "hw", "ether" , new_mac])
+subprocess.call(["ifconfig", interface , "up"])
 
 
