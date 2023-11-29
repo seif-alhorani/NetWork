a2d6e197b114a02c6a50ac8a61b4bd09ec5e1ae7
seif-alhorani <62389187+seif-alhorani@users.noreply.github.com>
2023-11-29 05:57:58 +0300

    Update FakeUpgrade.c

--- FakeUpgrade.c
+++ FakeUpgrade.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <wait.h>
+#include <windows.h>
 #include <math.h>
 int main()
 {
-    
+
     int i=0;
     while(i<=1){
         printf("Building dependency tree... Done\n");
@@ -25,14 +25,14 @@ int main()
         printf("Get:1 http://archive.ubuntu.com/ubuntu focal/universe amd64 libatomic1 amd64 10.3.0-1ubuntu1 [9,216 B]\n");
         printf("Fetched 9,216 B in 0s (0 B/s)\n");
         // Installing packages
-         sleep(4);
+         Sleep(400);
         printf("Selecting previously unselected package libatomic1:amd64.\n");
         printf("(Reading database ... 398613 files and directories currently installed.)\n");
         printf("Preparing to unpack .../libatomic1_10.3.0-1ubuntu1_amd64.deb ...\n");
         printf("Unpacking libatomic1:amd64 (10.3.0-1ubuntu1) ...\n");
         printf("Setting up libatomic1:amd64 (10.3.0-1ubuntu1) ...\n");
         // Processing triggers
-         sleep(4);
+         Sleep(400);
         printf("Processing triggers for libc-bin (2.31-0ubuntu9.16) ...\n");
         printf("Processing triggers for man-db (2.9.1-1) ...\n");
         // Additional lines for simulation
@@ -47,11 +47,11 @@ int main()
         // Prompt for configuration
         printf("Do you want to continue? [Y/n] ");
         // Downloading additional packages
-        sleep(4);
+        Sleep(400);
         printf("Get:2 http://archive.ubuntu.com/ubuntu focal/universe amd64 libgomp1 amd64 10.3.0-1ubuntu1 [111 kB]\n");
         printf("Fetched 111 kB in 0s (0 B/s)\n");
         // Installing additional packages
-        sleep(4);
+        Sleep(400);
         printf("Selecting previously unselected package libgomp1:amd64.\n");
         printf("Preparing to unpack .../libgomp1_10.3.0-1ubuntu1_amd64.deb ...\n");
         printf("Unpacking libgomp1:amd64 (10.3.0-1ubuntu1) ...\n");
@@ -60,7 +60,7 @@ int main()
         printf("Processing triggers for libc-bin (2.31-0ubuntu9.16) ...\n");
         // Additional lines for simulation
         // ... (you can continue adding more steps as needed)
-         sleep(4);
+         Sleep(400);
         printf("Unpacking libssh-4:amd64 (0.9.6-2ubuntu0.22.04.1) over (0.9.6-2build1) ...\n");
         printf("Preparing to unpack .../33-libcurl3-gnutls_7.81.0-1ubuntu1.14_amd64.deb ...\n");
         printf("Unpacking libcurl3-gnutls:amd64 (7.81.0-1ubuntu1.14) over (7.81.0-1ubuntu1.10) ...\n");
@@ -90,8 +90,11 @@ int main()
         printf("Unpacking apport (2.20.11-0ubuntu82.5) over (2.20.11-0ubuntu82.4) ...\n");
         printf("Preparing to unpack .../46-libctf0_2.38-4ubuntu2.3_amd64.deb ...\n");
         printf("Unpacking libctf0:amd64 (2.38-4ubuntu2.3) over (2.38-4ubuntu2.1) ..\n");
-        for(int j=0;j<=100;j++){
-            printf("Fetched %d kB in %ds (%d B/s)\n",j,rand(),j);
+        if(i%2==0){
+            for(int j=0;j<=100;j++){
+                Sleep(100);
+                printf("Fetched %d kB in %ds (%d B/s)\n",j,rand(),j);
+            }
         }
         printf("E: Failed to fetch http://security.ubuntu.com/ubuntu/pool/main/g/gcc-11/gcc-11-base_11.3.0-1ubuntu1%%7e22.04_amd64.deb  404  Not Found [IP: 91.189.91.82 80]\n");
         printf("E: Failed to fetch http://security.ubuntu.com/ubuntu/pool/main/g/gcc-11/cpp-11_11.3.0-1ubuntu1%%7e22.04_amd64.deb  404  Not Found [IP: 91.189.91.82 80]\n");
@@ -113,8 +116,8 @@ int main()
         printf("E: Failed to fetch http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc-devtools_2.35-0ubuntu3.1_amd64.deb  404  Not Found [IP: 91.189.91.82 80]\n");
         printf("E: Failed to fetch http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-libc-dev_5.15.0-71.78_amd64.deb  404  Not Found [IP: 91.189.91.82 80]\n");
         printf("E: Failed to fetch http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev_2.35-0ubuntu3.1_amd64.deb  404  Not Found [IP: 91.189.91.82 80]\n");
-       
-        sleep(4);
+
+        Sleep(40);
         i++;
     }
     printf("E: Unable to fetch some archives, maybe run apt-get update or try with --fix-missing?\n");
